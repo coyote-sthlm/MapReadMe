936fa3824385b2107aca1feaedba8dea892909d0
jacob broms <jacob.broms@sanocore.se>
2020-09-03 15:22:49 +0200

    haaands

--- js/main.js
+++ js/main.js
@@ -192,7 +192,7 @@ function initMap() {
 	    "elementType": "labels.text.fill",
 	    "stylers": [
 	      {
-	        "color": "#757575"
+	        "visibility": "off"
 	      }
 	    ]
 	  },
@@ -201,7 +201,7 @@ function initMap() {
 	    "elementType": "geometry",
 	    "stylers": [
 	      {
-	        "color": "#1CB14B"
+	        "color": "#000000"
 	      }
 	    ]
 	  },
@@ -219,7 +219,7 @@ function initMap() {
 	    "elementType": "labels.text.fill",
 	    "stylers": [
 	      {
-	        "color": "#3d3d3d"
+	        "visibility": "#3d3d3d"
 	      }
 	    ]
 	  }
@@ -246,36 +246,77 @@ function initMap() {
 	//map.mapTypes.set('styled_map', styledMapType);
 	//map.setMapTypeId('styled_map');
 
-	var iconBase = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/';
+	// var iconBase = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/';
 
-	var icons = {
-		parking: {
-			icon: iconBase + 'parking_lot_maps.png'
-		},
-		library: {
-			icon: iconBase + 'library_maps.png'
-		},
-		info: {
-			icon: iconBase + 'info-i_maps.png'
-		}
-	};
+	// var icons = {
+	// 	parking: {
+	// 		icon: iconBase + 'parking_lot_maps.png'
+	// 	},
+	// 	library: {
+	// 		icon: iconBase + 'library_maps.png'
+	// 	},
+	// 	info: {
+	// 		icon: iconBase + 'info-i_maps.png'
+	// 	}
+	// };
 
-	var features = [
+	var icon_metro = {
+		path: google.maps.SymbolPath.CIRCLE,
+		fillColor: '#1CB14B',
+		fillOpacity: 1.0,
+		strokeWeight: 0,
+		scale: 10
+	}
+
+	// {CIRCLE: 0, FORWARD_CLOSED_ARROW: 1, FORWARD_OPEN_ARROW: 2, BACKWARD_CLOSED_ARROW: 3, BACKWARD_OPEN_ARROW: 4}
+	var icon_home = {
+		path: google.maps.SymbolPath.CIRCLE,
+		fillColor: '#ff0000',
+		fillOpacity: 1.0,
+		strokeWeight: 0,
+		scale: 10
+	}
+
+	var home = new google.maps.LatLng(59.330473, 18.051249);
+
+	var metro_positions = [
 		{
-			position: new google.maps.LatLng(59.330473, 18.051249),
-			type: 'info'
+			id: '001',
+			position: new google.maps.LatLng(59.331211, 18.058717),
+			address: 'Vasagatan 1',
 		}
 	];
 
-	for (var i = 0; i < features.length; i++) {
+	// home
+
+	var infowindow = new google.maps.InfoWindow({
+		content: contentString
+	});
+
+	var marker = new google.maps.Marker({
+		position: home,
+		icon: icon_home,
+		map: map,
+		title: 'Index Stockholm'
+	});
+
+	marker.addListener('click', function() {
+		console.log(marker);
+		infowindow.open(map, marker);
+	});
+
+
+	// paper
+	for (var i = 0; i < metro_positions.length; i++) {
 		var marker = new google.maps.Marker({
-			position: features[i].position,
-			icon: icons[features[i].type].icon,
+			position: metro_positions[i].position,
+			icon: icon_metro, //icons[features[i].type].icon,
 			map: map,
-			title: 'Uluru (Ayers Rock)'
+			// label: 'wut',
+			title: metro_positions[i].address
 		});
 
-	// link for open in map?
+	 // link for open in map?
 
 	  var contentString = 'potato';
 
@@ -284,6 +325,7 @@ function initMap() {
 	  });
 
 	  marker.addListener('click', function() {
+	  	console.log(marker);
 	    infowindow.open(map, marker);
 	  });
 
