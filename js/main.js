d87829f8949778266efaddc29509336402e54de0
jacob broms <jacob.broms@sanocore.se>
2020-09-14 19:04:59 +0200

    weehoo

--- js/main.js
+++ js/main.js
@@ -1,151 +1,179 @@
 var rawPositions = [
- {
-   "ID": 1,
-   "Stadsdel": "City",
-   "Adress": "Vasagatan 1",
-   "Latitude": 59.331211,
-   "Longitude": 18.058717
- },
- {
-   "ID": 2,
-   "Stadsdel": "City",
-   "Adress": "Klarabergsviadukten 55",
-   "Latitude": 59.3306418,
-   "Longitude": 18.056534
- },
- {
-   "ID": 3,
-   "Stadsdel": "City",
-   "Adress": "Vasagatan 28",
-   "Latitude": 59.329324,
-   "Longitude": 18.068581
- },
- {
-   "ID": 4,
-   "Stadsdel": "City",
-   "Adress": "Sveavägen 41",
-   "Latitude": 59.338763,
-   "Longitude": 59.338763
- },
- {
-   "ID": 5,
-   "Stadsdel": "City",
-   "Adress": "Sveavägen 53",
-   "Latitude": 59.340172,
-   "Longitude": 18.058825
- },
- {
-   "ID": 6,
-   "Stadsdel": "City",
-   "Adress": "Sveavägen 65",
-   "Latitude": 59.341685,
-   "Longitude": 18.057208
- },
- {
-   "ID": 7,
-   "Stadsdel": "Odenplan",
-   "Adress": "Odengatan 57",
-   "Latitude": 59.345017,
-   "Longitude": 18.059869
- },
- {
-   "ID": 8,
-   "Stadsdel": "Odenplan",
-   "Adress": "Karlbergsvägen 8",
-   "Latitude": 59.343213,
-   "Longitude": 18.050295
- },
- {
-   "ID": 9,
-   "Stadsdel": "Odenplan",
-   "Adress": "Karlbergsvägen (Odenplan)",
-   "Latitude": 59.343,
-   "Longitude": 18.05
- },
- {
-   "ID": 10,
-   "Stadsdel": "Vasastan",
-   "Adress": "St:eriksplan 10",
-   "Latitude": 59.339835,
-   "Longitude": 18.036226
- },
- {
-   "ID": 11,
-   "Stadsdel": "Kungsholmen",
-   "Adress": "St:eriksgatan 45 ",
-   "Latitude": 59.334719,
-   "Longitude": 18.032365
- },
- {
-   "ID": 12,
-   "Stadsdel": "Kungsholmen",
-   "Adress": "Fridhemsgatan 17",
-   "Latitude": 59.332615,
-   "Longitude": 18.02788
- },
- {
-   "ID": 13,
-   "Stadsdel": "Kungsholmen",
-   "Adress": "Drottningholmsvägen 22",
-   "Latitude": 59.332069,
-   "Longitude": 18.030948
- },
- {
-   "ID": 14,
-   "Stadsdel": "Marieberg",
-   "Adress": "Västerbroplan",
-   "Latitude": 59.3285,
-   "Longitude": 18.0214
- },
- {
-   "ID": 15,
-   "Stadsdel": "Södermalm",
-   "Adress": "Helenborgsgatan 27",
-   "Latitude": 59.318687,
-   "Longitude": 18.032894
- },
- {
-   "ID": 16,
-   "Stadsdel": "Södermalm",
-   "Adress": "Långholmsgatan 15",
-   "Latitude": 59.316789,
-   "Longitude": 18.033552
- },
- {
-   "ID": 17,
-   "Stadsdel": "Södermalm",
-   "Adress": "Ringvägen 8",
-   "Latitude": 59.317736,
-   "Longitude": 18.04996
- },
- {
-   "ID": 18,
-   "Stadsdel": "Södermalm",
-   "Adress": "Rosenlundsgatan 7",
-   "Latitude": 59.316812,
-   "Longitude": 18.055916
- },
- {
-   "ID": 19,
-   "Stadsdel": "Södermalm",
-   "Adress": "Götgatan 78",
-   "Latitude": 59.307713,
-   "Longitude": 18.076656
- },
- {
-   "ID": 20,
-   "Stadsdel": "Södermalm",
-   "Adress": "Tjärhovsplan",
-   "Latitude": 59.3163,
-   "Longitude": 18.0837
- },
- {
-   "ID": 21,
-   "Stadsdel": "Södermalm",
-   "Adress": "Ringvägen 117",
-   "Latitude": 59.307763,
-   "Longitude": 18.0754
- }
+  {
+    "ID": 1,
+    "Stadsdel": "City",
+    "Adress": "Vasagatan 1",
+    "Latitude": 59.331211,
+    "Longitude": 18.058717
+  },
+  {
+    "ID": 2,
+    "Stadsdel": "City",
+    "Adress": "Klarabergsviadukten 55",
+    "Latitude": 18.058717,
+    "Longitude": 18.056534
+  },
+  {
+    "ID": 3,
+    "Stadsdel": "City",
+    "Adress": "Vasagatan 28",
+    "Latitude": 59.329324,
+    "Longitude": 18.068581
+  },
+  {
+    "ID": 4,
+    "Stadsdel": "City",
+    "Adress": "Sveavägen 41",
+    "Latitude": 59.338763,
+    "Longitude": 18.060139
+  },
+  {
+    "ID": 5,
+    "Stadsdel": "City",
+    "Adress": "Sveavägen 53",
+    "Latitude": 59.340172,
+    "Longitude": 18.058825
+  },
+  {
+    "ID": 6,
+    "Stadsdel": "City",
+    "Adress": "Sveavägen 65",
+    "Latitude": 59.341685,
+    "Longitude": 18.057208
+  },
+  {
+    "ID": 7,
+    "Stadsdel": "Odenplan",
+    "Adress": "Odengatan 57",
+    "Latitude": 59.345017,
+    "Longitude": 18.059869
+  },
+  {
+    "ID": 8,
+    "Stadsdel": "Odenplan",
+    "Adress": "Karlbergsvägen 8",
+    "Latitude": 59.343213,
+    "Longitude": 18.050295
+  },
+  {
+    "ID": 9,
+    "Stadsdel": "Odenplan",
+    "Adress": "Karlbergsvägen (Odenplan)",
+    "Latitude": 59.343,
+    "Longitude": 18.05
+  },
+  {
+    "ID": 10,
+    "Stadsdel": "Vasastan",
+    "Adress": "St:Eriksplan 10",
+    "Latitude": 59.339835,
+    "Longitude": 18.036226
+  },
+  {
+    "ID": 11,
+    "Stadsdel": "Kungsholmen",
+    "Adress": "St:Eriksgatan 45",
+    "Latitude": 59.334719,
+    "Longitude": 18.032365
+  },
+  {
+    "ID": 12,
+    "Stadsdel": "Kungsholmen",
+    "Adress": "Fridhemsgatan 17",
+    "Latitude": 59.332615,
+    "Longitude": 18.02788
+  },
+  {
+    "ID": 13,
+    "Stadsdel": "Kungsholmen",
+    "Adress": "Drottningholmsvägen 22",
+    "Latitude": 59.332069,
+    "Longitude": 18.030948
+  },
+  {
+    "ID": 14,
+    "Stadsdel": "Marieberg",
+    "Adress": "Västerbroplan",
+    "Latitude": 59.3285,
+    "Longitude": 18.0214
+  },
+  {
+    "ID": 15,
+    "Stadsdel": "Södermalm",
+    "Adress": "Helenborgsgatan 27",
+    "Latitude": 59.318687,
+    "Longitude": 18.032894
+  },
+  {
+    "ID": 16,
+    "Stadsdel": "Södermalm",
+    "Adress": "Långholmsgatan 15",
+    "Latitude": 59.316789,
+    "Longitude": 18.033552
+  },
+  {
+    "ID": 17,
+    "Stadsdel": "Södermalm",
+    "Adress": "Ringvägen 8",
+    "Latitude": 59.317736,
+    "Longitude": 18.04996
+  },
+  {
+    "ID": 18,
+    "Stadsdel": "Södermalm",
+    "Adress": "Rosenlundsgatan 7",
+    "Latitude": 59.316812,
+    "Longitude": 18.055916
+  },
+  {
+    "ID": 19,
+    "Stadsdel": "Södermalm",
+    "Adress": "Götgatan 78",
+    "Latitude": 59.307713,
+    "Longitude": 18.076656
+  },
+  {
+    "ID": 20,
+    "Stadsdel": "Södermalm",
+    "Adress": "Tjärhovsplan",
+    "Latitude": 59.3163,
+    "Longitude": 18.0837
+  },
+  {
+    "ID": 21,
+    "Stadsdel": "Södermalm",
+    "Adress": "Ringvägen 117",
+    "Latitude": 59.307763,
+    "Longitude": 18.0754
+  },
+  {
+    "ID": 22,
+    "Stadsdel": "Henriksdalsberget",
+    "Adress": "Hendriksdalsringen 115",
+    "Latitude": 59.311039,
+    "Longitude": 18.117287
+  },
+  {
+    "ID": 23,
+    "Stadsdel": "Södermalm",
+    "Adress": "Katarinabangata 13",
+    "Latitude": 59.312245,
+    "Longitude": 18.074728
+  },
+  {
+    "ID": 24,
+    "Stadsdel": "Södermalm",
+    "Adress": "Ringvägen 96",
+    "Latitude": 59.307474,
+    "Longitude": 18.074162
+  },
+  {
+    "ID": 25,
+    "Stadsdel": "Södermalm",
+    "Adress": "Swedenborgsgatan 20",
+    "Latitude": 59.313898,
+    "Longitude": 18.064797
+  }
 ];
 
 function initMap() {
@@ -393,7 +421,7 @@ function initMap() {
 
 	var icon_metro = {
 		path: google.maps.SymbolPath.CIRCLE,
-		fillColor: '#1CB14B',
+		fillColor: '#ffbc4d',
 		fillOpacity: 1.0,
 		strokeWeight: 0,
 		scale: 5
@@ -402,7 +430,7 @@ function initMap() {
 	// {CIRCLE: 0, FORWARD_CLOSED_ARROW: 1, FORWARD_OPEN_ARROW: 2, BACKWARD_CLOSED_ARROW: 3, BACKWARD_OPEN_ARROW: 4}
 	var icon_home = {
 		path: google.maps.SymbolPath.CIRCLE,
-		fillColor: '#ff0000',
+		fillColor: '#00ca57',
 		fillOpacity: 1.0,
 		strokeWeight: 0,
 		scale: 8
@@ -410,7 +438,8 @@ function initMap() {
 
 	//$('#aboutModal').modal('show');
 
-	var home = new google.maps.LatLng(59.330473, 18.051249);
+	var home = new google.maps.LatLng(59.330473, 18.051249); // index
+	var alt_loc = new google.maps.LatLng(59.324672, 18.087397); // other
 
 	var metro_positions = rawPositions.map(convertToMapPositions)
 
@@ -437,8 +466,17 @@ function initMap() {
 	});
 
 	home_marker.addListener('click', function() {
-		// infowindow.open(map, home_marker);
 		$('#aboutModal').modal('show');
+	});
+
+	var alt_loc_marker = new google.maps.Marker({
+		position: alt_loc,
+		icon: icon_home,
+		map: map,
+	});
+
+	alt_loc_marker.addListener('click', function() {
+		$('#getMeModal').modal('show');
 	});
 
 	// paper
@@ -487,7 +525,7 @@ $(document).ready(function(){
 	  let imgUrl = '/img/bins/' + pad(modal.data('marker-id'),3) + '.jpg';
 
 	  modal.find('img').attr('src', imgUrl);
-	  
+	  modal.find('.modal-body').css('background-image', 'url(' + imgUrl + ')');
 	})
 
 });
